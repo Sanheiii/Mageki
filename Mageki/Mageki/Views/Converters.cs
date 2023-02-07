08afecd2119f117ae06ca0961712bde79ee25215
三黑 <w953934508@outlook.com>
2023-02-08 01:02:52 +0800

    隐藏开启特定项后变得无用的设置项

--- Mageki/Mageki/Views/Converters.cs
+++ Mageki/Mageki/Views/Converters.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
@@ -94,6 +95,18 @@ namespace Mageki.Views
                 return (LeverMoveMode)i;
             }
             else return default(LeverMoveMode);
+        }
+    }
+    public class AndConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.All(value => (bool)value);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
